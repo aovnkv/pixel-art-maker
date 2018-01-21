122e4aaef9d45b6b7b86228df61cf0bb4500c98b
clitorrent <nofake@mail.ru>
2018-01-21 15:01:00 +0400

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,33 +1,70 @@
-// Select color input
-// Select size input
 
-// When size is submitted by the user, call makeGrid()
+// set some const variables and default color for a colorwell
+
+const colorWell = document.getElementById("colorPicker");
+let color = "#000000";
+
+// make a function that will build the grid with height and width params
+// append the grid to table element
 
 function makeGrid(height, width) {
 
 
-	var grid = "";
+	let grid = "";
 
-	for ( var h = 1; h <= height; h++ ) {
+	for ( let h = 1; h <= height; h++ ) {
 
-		var col = "";
+		let col = "";
 
-		for ( var w = 1; w <= width; w++ ) {
+		for ( let w = 1; w <= width; w++ ) {
 
 			col += "<td></td>";
-			console.log();
+
 		};
 
 		grid += "<tr>" + col + "</tr>";
 
 	};
 
-
-	console.log(grid);
 	$(grid).appendTo("table");
-	//$(cols).appendTo("tr");
+
 }
 
-makeGrid(5, 3);
+// making an event handler on submit button
+
+$('input[type="submit"]').on("click", function(e) {
+
+	e.preventDefault(); //preventing a default handling for click event
+	$("tr").remove(); //reset our previos grid, if it was
+	let hval = "";
+	let wval = "";
+	hval = $("#input_height").val();
+	wval = $("#input_width").val();
+	makeGrid(hval, wval);
+	$("td").css("background-color", "white");
+
+})
+
+// set a new color for paint on colorwell change
+
+$("#colorPicker").on("change", function(){
+
+	color = colorWell.value;
+	//console.log(color);
+})
+
+// making an event handler with event delegation for painting grid cells by clicking them
+
+$("#pixel_canvas").on("click", "td", function(evt){
+
+	$(evt.target).css("background-color", color);
+})
+
+// set the event handler for reset the grid button
+
+$("button").on("click", function(e){
+	e.preventDefault;
+	$("td").css("background-color", "white");
+})
 
 
